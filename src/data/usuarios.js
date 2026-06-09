241d8fcd4bdbf329df56ba28fd4889f1ec885cac
joelalan13 <joel-alan13@hotmail.com>
2026-06-08 21:20:41 -0300

    creacion de data de un array con usuarios

---
+++ src/data/usuarios.js
@@ -0,0 +1,13 @@
+const usuariosRegistrados = [
+    {
+        id:1,
+        nombre:"Esteban",
+        apellido:"Estebanquito",
+        userName:"Esteban777",
+        biblioteca:[2,3,4,10,11,12],
+        favoritos:[1,2,10],
+        listaDeDeseados:[5,6,7,8,9]
+    },
+]
+
+export default usuariosRegistrados
