2f55257bcae1ce06cf6d8edb453ec1a42c57492f
joelalan13 <joel-alan13@hotmail.com>
2026-06-10 22:29:09 -0300

    arreglo de polimorfismo en filtros de juegos

--- src/pages/Biblioteca.jsx
+++ src/pages/Biblioteca.jsx
@@ -30,22 +30,15 @@ function Biblioteca({temaActual}){
 
     const categorias = [...new Set(juegosOrdenados.flatMap(juego => juego.genero))]
 
-    const juegosFiltrados = juegosOrdenados.filter((juego) => {
+    const listaDeJuegos = favoritos ? listaDeDeseados : juegosOrdenados;
+
+    const juegosFiltrados = listaDeJuegos.filter((juego) => {
 
         const porTitulo = juego.titulo.toLowerCase().includes(buscador.toLowerCase())
         const porCategoria = categoriaSelec == "" || juego.genero.includes(categoriaSelec)
 
 
         return porTitulo && porCategoria
-        }
-    )
-    const juegosDeseadosFiltrados = listaDeDeseados.filter((juego) => {
-
-            const porTitulo = juego.titulo.toLowerCase().includes(buscador.toLowerCase())
-            const porCategoria = categoriaSelec == "" || juego.genero.includes(categoriaSelec)
-
-
-            return porTitulo && porCategoria
         }
     )
     
@@ -73,8 +66,7 @@ function Biblioteca({temaActual}){
             <div className={`p-5 text-center rounded border border-secondary ${temaActual === 'primary' ? 'bg-primary' : 'bg-dark'}`}>
             {/* Grid de juegos */}
             <Row className="g-4">
-                {favoritos === false ? (
-                    juegosFiltrados.map(juego => (
+                    {juegosFiltrados.map(juego => (
                         <Col key={juego.id} xs={12} sm={6} md={4}>
                             <JuegoCard titulo={juego.titulo} generos={juego.genero} imagen={juego.imagen}>
                                 {/* Los botones entran como children */}
@@ -90,22 +82,7 @@ function Biblioteca({temaActual}){
                                 </Row>
                             </JuegoCard>
                         </Col>
-                    ))
-                ) : (
-                    juegosDeseadosFiltrados.map(juego => (
-                        <Col key={juego.id} xs={12} sm={6} md={4}>
-                            <JuegoCard titulo={juego.titulo} generos={juego.genero} imagen={juego.imagen}>
-                                <Row className="g-2 mt-2">
-                                    <Col xs={12}>
-                                        <Link to={`/productos/${juego.id}`} className="text-decoration-none">
-                                            <button className='btn-biblioteca w-100 btn btn-outline-info btn-sm'>Ir a Tienda</button>
-                                        </Link>
-                                    </Col>
-                                </Row>
-                            </JuegoCard>
-                        </Col>
-                    ))
-                )}
+                    ))}
             </Row>
             </div> 
         </Container>
